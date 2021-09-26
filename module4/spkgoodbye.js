7aa89a9e8c2f990f2aab2ee92cf93585f76b47bc
Priya-cse <89922343+Priya-cse@users.noreply.github.com>
2021-09-26 22:23:51 +0530

    Update spkgoodbye.js

--- module4/spkgoodbye.js
+++ module4/spkgoodbye.js
@@ -1,7 +1,7 @@
 (function(window) {
 	var byeSpeaker = new Object();
 	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
+	byeSpeaker.speak = function speak(name) {
   		console.log(speakWord + " " + name);
 	}
 	
