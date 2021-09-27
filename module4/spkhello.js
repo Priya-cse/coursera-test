e4e0ed098b7160b245e054208eee3f73b2b889d9
Priya-cse <89922343+Priya-cse@users.noreply.github.com>
2021-09-27 14:25:00 +0530

    Update spkhello.js

--- module4/spkhello.js
+++ module4/spkhello.js
@@ -1,9 +1,6 @@
-(function (window) {
-  var speakWord = "Hello";
-  var helloSpeaker = {
-    speak: function (name) {
-      console.log(speakWord + " " + name);
-    }
-  };
-  window.helloSpeaker = helloSpeaker;
-})(window);
+var helloSpeaker = {
+    speakWord : "Hello ",
+    speak : function(name) {
+        console.log(this.speakWord + " " + name);
+    } 
+}
