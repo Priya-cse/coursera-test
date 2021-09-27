ab5819899a18b456216c0abf66c0c0c6d4d1202a
Priya-cse <89922343+Priya-cse@users.noreply.github.com>
2021-09-27 14:26:18 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,12 +1,25 @@
-(function (window) {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
 
-  for (var i in names) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
+    var names = [
+        "Yaakov", 
+        "John", 
+        "Jen", 
+        "Jason", 
+        "Paul", 
+        "Frank", 
+        "Larry", 
+        "Paula", 
+        "Laura", 
+        "Jim",
+        "jese"
+    ];
+    for (var i = 0; i < names.length; i++) {
+      var firstLetterLowerCase = names[i].charAt(0).toLowerCase();
+      if (firstLetterLowerCase === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
     }
-  }
-})(window);
+    
+})();
